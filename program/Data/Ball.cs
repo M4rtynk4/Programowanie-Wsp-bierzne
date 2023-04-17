91d58cc2f3af687f3fac8d42271e617565da0a3f
M4rtynk4 <242364@edu.p.lodz.pl>
2023-04-17 20:12:58 +0200

    cos

---
+++ program/Data/Ball.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Ball : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private int _xposition, _yposition, _radius;
+        private int[] Speed = new int[2];
+
+        public Ball(int xposition, int yposition, int radius)
+        {
+            _xposition = xposition;
+            _yposition = yposition;
+            _radius = radius;
+
+            int xSpeed = 0;
+
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public int Xposition
+        {
+
+            get { return _xposition; }
+            set
+            {
+                _xposition = value;
+                OnPropertyChanged("Xposition");
+            }
+        }
+        public int Yposition
+        {
+
+            get { return _yposition; }
+            set
+            {
+                _yposition = value;
+                OnPropertyChanged("Yposition");
+            }
+
+        }
+        public int Radius => _radius;
+
+    
+
+
+    }
+
+
+
+}
---
+++ program/Data/DataApi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+
+namespace Data
+{
+
+    public abstract class DataAbstractApi
+    {
+        public abstract Ball CreateBall(int xposition, int yposition, int radius);
+     //   public abstract List<Ball> GetBall();
+       // public abstract int BoardWidth { get; }
+       // public abstract int BoardHeight { get; }
+       // public abstract void Disable();
+       // public abstract void CreateBoard(int width, int height);
+             public static DataAbstractApi CreateAPI()
+        {
+            return new DataAPI();
+        }
+         internal sealed class DataAPI : DataAbstractApi
+        {
+            public override Ball CreateBall(int xposition, int yposition, int radius)
+            {
+                return new Ball(xposition, yposition, radius);
+            }
+
+            
+
+        }
+
+     }
+
+    
+}
+

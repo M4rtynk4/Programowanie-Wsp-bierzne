3ca912df9b66ee66a531d41f481cc220b68c32f6
Mateusz Barański <mateuszbaranski6@onet.pl>
2023-06-14 01:07:31 +0200

    Threads zamienione na timer

--- program/Data/Log.cs
+++ program/Data/Log.cs
@@ -1,45 +1,48 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.IO;
-using System.Threading.Tasks;
+using System.Timers;
 
 namespace Data
 {
-    public class Log 
+    public class Log
     {
         private BlockingCollection<string> buffer = new BlockingCollection<string>();
-        private Task fileWriter;
         private StreamWriter sw;
+        private System.Timers.Timer timer;
+        private object lockObject = new object();
+
         public Log()
         {
-            
-            fileWriter = new Task(() => Write());
-            fileWriter.Start();
-             
+            timer = new System.Timers.Timer(1000);
+            timer.Elapsed += TimerElapsed;
+            timer.Start();
         }
 
-   
         public void AddToBuffer(Ball ball)
         {
-                string time = DateTime.Now.ToString("h:mm:ss.fff tt");
-                string log = $"{time} Ruch piłki numer {ball.Id}: Pozycja: x={Math.Round(ball.x, 5)}, y={Math.Round(ball.y, 5)}, Przemieszczenie: x={Math.Round(ball.XSpeed, 5)}, y={Math.Round(ball.YSpeed, 5)}";
-                buffer.Add(log);
-            
+            string time = DateTime.Now.ToString("h:mm:ss.fff tt");
+            string log = $"{time} Ruch piłki numer {ball.Id}: Pozycja: x={Math.Round(ball.x, 5)}, y={Math.Round(ball.y, 5)}, Przemieszczenie: x={Math.Round(ball.XSpeed, 5)}, y={Math.Round(ball.YSpeed, 5)}";
+            buffer.Add(log);
         }
 
-       
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Write();
+        }
 
         public void Write()
         {
-            
-                using (sw = new StreamWriter("../../../../Data/log.txt", append: false))
+            lock (lockObject)
+            {
+                using (sw = new StreamWriter("../../../../Data/log.txt", append: true))
                 {
-                    foreach (string log in buffer.GetConsumingEnumerable())
+                    while (buffer.TryTake(out string log))
                     {
                         sw.WriteLine(log);
                     }
                 }
-            
+            }
         }
     }
 }
